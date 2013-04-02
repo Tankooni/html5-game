002da1fbbe54bf3b8fa2aa7f237f9e7fae6abdf5
Matthew Lindsay <Matthew Lindsay@localhost>
2013-04-02 18:55:43 -0400

    Obligatory commit

--- js/audios.js
+++ js/audios.js
@@ -1,79 +1,58 @@
-//var audioContext;
-//var soundBuffer = null;
-//var soundIsReady = false;
-//
-//function audioInit()
-//{
-//    try
-//    {
-//        audioContext = new webkitAudioContext();
-//    }
-//    catch(e)
-//    {
-//        alert('Web Audio API is not supported in this browser');
-//    }
-//}
-//    
-//function loadSound(url)
-//{
-//    var request = new XMLHttpRequest();
-//    request.open('GET', url, true);
-//    request.responseType = 'arraybuffer';
-//    
-//    // Decode asynchronously
-//    request.onload = function()
-//    {
-//        audioContext.decodeAudioData(request.response, function(buffer)
-//        {
-//          soundBuffer = buffer;
-//        }, onError);
-//        soundIsReady = true;
-//        console.warn('Sound Loaded?');
-//    
-//    request.send();
-//    console.warn(request.readyState);
-//}
-//    
-//function onError()
-//{
-//    alert('error');
-//}
-//    
-//function playSound()
-//{
-//    console.warn('playing sound');
-//    var source = audioContext.createBufferSource(); // creates a sound source
-//    source.buffer = soundBuffer;                    // tell the source which sound to play
-//    source.connect(audioContext.destination);       // connect the source to the context's destination (the speakers)
-//    source.noteOn(0);                         // play the source now
-//}
-
-
-//window.onload = init();
-
-var context;
-var bufferLoader;
-
-function audioInit()
+function AudioManager(music, sounds)
 {
-    context = new webkitAudioContext();
+    this.context = new webkitAudioContext();
+    this.Music={};
+    this.Sounds={};
+    
+    var bufferLoader;
     
     bufferLoader = new BufferLoader(
-        context,
-        [
-            'lib/audio/sound.wav',
-        ],
+        this.context,
+        music,
         finishedLoading
     );
     bufferLoader.load();
     
+    
+    function finishedLoading(bufferList)
+    {
+        alert('Sound Loaded');
+        var source1 = this.context.createBufferSource();
+        source1.buffer = bufferList[0];
+        source1.connect(this.context.destination);
+        source1.noteOn(0);
+    }
+    
+    this.PlayLoop = function(song)
+    {
+        
+    }
 }
 
-function finishedLoading(bufferList)
-{
-    alert('Sound Loaded');
-    var source1 = context.createBufferSource();
-    source1.buffer = bufferList[0];
-    source1.connect(context.destination);
-    source1.noteOn(0);
-}
+
+
+// var context;
+// var bufferLoader;
+// 
+// function audioInit()
+// {
+//     context = new webkitAudioContext();
+//     
+//     bufferLoader = new BufferLoader(
+//         context,
+//         [
+//             'lib/audio/sound.wav',
+//         ],
+//         finishedLoading
+//     );
+//     bufferLoader.load();
+// }
+// 
+// function finishedLoading(bufferList)
+// {
+//     alert('Sound Loaded');
+//     var source1 = context.createBufferSource();
+//     source1.buffer = bufferList[0];
+//     source1.connect(context.destination);
+//     source1.noteOn(0);
+// }
